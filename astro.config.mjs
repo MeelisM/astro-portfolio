1e8c31ab200c3bc098c1939b22882a8c5c3bfcf2
Meelis Mumm <meelis.developer@gmail.com>
2025-03-07 16:48:15 +0200

    Restrucure textGlitchAnimation usage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.meelis.dev",
   experimental: {
     svg: true,
   },
   vite: {
     plugins: [tailwindcss()],
     build: {
+      minify: true,
       rollupOptions: {
         output: {
           manualChunks: {},
