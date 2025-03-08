cb54200f92b16480342940cce9f08047ec068c0d
Meelis Mumm <73930233+MeelisM@users.noreply.github.com>
2025-03-08 12:35:16 +0200

    Format Terraform main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -178,7 +178,7 @@ resource "aws_cloudfront_distribution" "website_distribution" {
   origin {
     domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
     origin_id   = "S3-${var.subdomain_name}"
-    
+
     # Replace s3_origin_config with origin_access_control_id
     origin_access_control_id = aws_cloudfront_origin_access_control.website_oac.id
   }
